0585b4822914f851c9eab6b6506fc97534c99f16
Andy Lawrence <andy.lawrence@gmail.com>
2012-07-28 18:52:58 -0400

    cleanup

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,7 +3,8 @@ class LoginController < ApplicationController
   end
   
   def login
-    session[:user] = User.find_by_username params[:username]    
+    session[:user] = User.find_by_username params[:username]  
+    #if user exists, we're good  
     redirect_to '/home'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :password, :username, :account_balance
   validates_presence_of :username
-  
 end
