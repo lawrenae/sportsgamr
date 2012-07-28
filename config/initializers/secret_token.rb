bf497992a56daf415f298488f9c55a5332ccb3e1
Andy Lawrence <andy.lawrence@gmail.com>
2012-07-28 12:50:17 -0400

    basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportsgamr::Application.config.secret_token = 'ae8bd078d656ff0b79bacf715648108f5984b3c16df0853edef25c6aefab933973bafbc55b618b3fdbe3eb002b518f9cd44754f66afb4f9b73033a10c5075e6f'
