779c7ba337e0b174a10575ecbe2135c146409c16
Andy Lawrence <andy.lawrence@gmail.com>
2012-07-29 12:49:12 -0400

    added a matt user

--- db/migrate/20120728214547_add_a_few_users.rb
+++ db/migrate/20120728214547_add_a_few_users.rb
@@ -1,7 +1,7 @@
 class AddAFewUsers < ActiveRecord::Migration
   def up
     User.create!(:username=>'austin', :password=>'powers', :first_name=>'Austin', :last_name=>"Hackett", :account_balance=>500)
-    User.create!(:username=>'nick', :password=>'nick', :first_name=>'Nick', :last_name=>"Weintge", :account_balance=>741)
+    User.create!(:username=>'matt', :password=>'matt', :first_name=>'Matt', :last_name=>"Weintge", :account_balance=>741)
   end
 
   def down
